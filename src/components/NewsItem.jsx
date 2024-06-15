1bb46205e96d211090ad496aed1d90f2622b223e
moradoulhaj <moradoulhaj.off@gmail.com>
2024-06-15 22:48:29 +0100

    desplay data

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function NewsItem(props) {
-    return (
-      <div>h</div>
-//     <div><div className="card" style="width: 18rem;">
-//     <img src={} className="card-img-top" alt="...">
-//     <div className="card-body">
-//       <h5 className="card-title">Card title</h5>
-//       <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//       <a href="#" className="btn btn-primary">Go somewhere</a>
-//     </div>
-//   </div></div>
-  )
+  return (
+    <div class="card" style={{maxWidth: "300px"}}>
+      <img src={props.src} class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">{props.title}</h5>
+        <p class="card-text">{props.description}</p>
+        <a href={props.url} class="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default NewsItem
+export default NewsItem;
